eb3a898c03862d89083cc6d00df7c0f091230234
Jonathan Leonard <johanatan@gmail.com>
2016-05-03 15:33:09 -0400

    Added a lower level interface for KV implementations and uncooked access to kv implementations... implemented the higher level interface over the raw interface

---
+++ pkg/storage/generic/interfaces.go
@@ -0,0 +1,70 @@
+
+package generic
+
+import (
+	//"time"
+
+	"k8s.io/kubernetes/pkg/watch"
+
+	"golang.org/x/net/context"
+)
+type RawObject struct {
+	Data    []byte
+	Version uint64
+	TTL     int64
+}
+
+type RawFilterFunc func( raw *RawObject ) (bool, error)
+type RawEvent struct {
+	Type    watch.EventType
+	Raw     RawObject
+}
+type InterfaceRawWatch interface {
+	Stop()
+	ResultChan() <-chan RawEvent
+}
+
+type UpdateFunc func(raw *RawObject) error
+
+type InterfaceRaw interface {
+	Backends(ctx context.Context) []string
+	
+	Create(ctx context.Context, key string, data []byte, raw *RawObject, ttl uint64) error
+	
+	// Delete removes the specified key and returns the value that existed at that spot.
+	// If key didn't exist, it will return NotFound storage error.
+	Delete(ctx context.Context, key string, raw *RawObject, preconditions RawFilterFunc) error
+
+	// Watch begins watching the specified key. Events are decoded into API objects,
+	// and any items passing 'filter' are sent down to returned watch.Interface.
+	// resourceVersion may be used to specify what version to begin watching,
+	// which should be the current resourceVersion, and no longer rv+1
+	// (e.g. reconnecting without missing any updates).
+	Watch(ctx context.Context, key string, resourceVersion string) (InterfaceRawWatch, error)
+
+	// WatchList begins watching the specified key's items. Items are decoded into API
+	// objects and any item passing 'filter' are sent down to returned watch.Interface.
+	// resourceVersion may be used to specify what version to begin watching,
+	// which should be the current resourceVersion, and no longer rv+1
+	// (e.g. reconnecting without missing any updates).
+	WatchList(ctx context.Context, key string, resourceVersion string) (InterfaceRawWatch, error)
+
+	// Get unmarshals json found at key into objPtr. On a not found error, will either
+	// return a zero object of the requested type, or an error, depending on ignoreNotFound.
+	// Treats empty responses and nil response nodes exactly like a not found error.
+	Get(ctx context.Context, key string, raw *RawObject, ignoreNotFound bool) error
+
+	// GetToList unmarshals json found at key and opaque it into *List api object
+	// (an object that satisfies the runtime.IsList definition).
+	GetToList(ctx context.Context, key string, rawList *[]RawObject) (uint64, error)
+
+	// List unmarshalls jsons found at directory defined by key and opaque them
+	// into *List api object (an object that satisfies runtime.IsList definition).
+	// The returned contents may be delayed, but it is guaranteed that they will
+	// be have at least 'resourceVersion'.
+	List(ctx context.Context, key string, resourceVersion string, rawList *[]RawObject) (uint64, error)
+
+	// TODO: Figure out what, exactly, must be done to facilitate GuaranteedUpdate
+	// from the raw layer
+	GuaranteedUpdate(ctx context.Context, key string, ignoreNotFound bool, tryUpdate UpdateFunc) error
+}
---
+++ pkg/storage/generic_implementation.go
@@ -0,0 +1,419 @@
+package storage
+
+import (
+	"errors"
+	"fmt"
+	"path"
+	"reflect"
+	"strconv"
+	"strings"
+	"sync/atomic"
+	"time"
+	
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/api/meta"
+	"k8s.io/kubernetes/pkg/api/unversioned"
+	"k8s.io/kubernetes/pkg/conversion"
+	"k8s.io/kubernetes/pkg/runtime"
+	"k8s.io/kubernetes/pkg/storage/generic"
+	"k8s.io/kubernetes/pkg/util"
+	"k8s.io/kubernetes/pkg/watch"
+
+	"github.com/golang/glog"
+	"golang.org/x/net/context"
+)
+
+type genericWrapper struct {
+	generic     generic.InterfaceRaw
+	versioner   Versioner
+	codec       runtime.Codec
+	copier      runtime.ObjectCopier
+	pathPrefix  string
+}
+
+func NewGenericWrapper(raw generic.InterfaceRaw, codec runtime.Codec, prefix string) Interface {
+	return &genericWrapper{
+		generic:    raw,
+		versioner:  APIObjectVersioner{},
+		codec:      codec,
+		copier:     api.Scheme,
+		pathPrefix: path.Join("/", prefix),
+	}
+} 
+
+
+func(s *genericWrapper) Backends(ctx context.Context) []string {
+	return s.Backends(ctx)
+}
+
+func(s *genericWrapper) Versioner() Versioner {
+	return s.versioner
+}
+
+func(s *genericWrapper) Create(ctx context.Context, key string, obj, out runtime.Object, ttl uint64) error {
+	trace := util.NewTrace("GenericWrapper::Create " + getTypeName(obj))
+	defer trace.LogIfLong(250 * time.Millisecond)
+	if ctx == nil {
+		glog.Errorf("Context is nil")
+	}
+	key = s.prefixKey(key)
+	data, err := runtime.Encode(s.codec, obj)
+	trace.Step("Object encoded")
+	if err != nil {
+		return err
+	}
+	if version, err := s.versioner.ObjectResourceVersion(obj); err == nil && version != 0 {
+		return errors.New("resourceVersion may not be set on objects to be created")
+	}
+	trace.Step("Version checked")
+	
+	raw_out := generic.RawObject{} 
+	err = s.generic.Create(ctx, key, data, &raw_out, ttl)
+	if err != nil {
+		return err
+	}
+	if out != nil {
+		if _, err := conversion.EnforcePtr(out); err != nil {
+			panic("unable to convert output object to pointer")
+		}
+		err = s.extractObj(raw_out, err, out, false)
+	}
+	return err
+}
+
+func(s *genericWrapper) Delete(ctx context.Context, key string, out runtime.Object, preconditions *Preconditions) error {
+	if ctx == nil {
+		glog.Errorf("Context is nil")
+	}
+	key = s.prefixKey(key)
+	v, err := conversion.EnforcePtr(out)
+	if err != nil {
+		panic("unable to convert output object to pointer")
+	}
+	obj := reflect.New(v.Type()).Interface().(runtime.Object)
+	
+	var filter generic.RawFilterFunc
+	if preconditions != nil {
+		filter = func(raw *generic.RawObject) (bool, error) {
+			err = s.extractObj(*raw, nil, obj, false)
+			if err != nil {
+				return false, err
+			}
+			if err := checkPreconditions(key, 0, preconditions, obj); err != nil {
+				return false, err
+			}
+			return true, nil
+		}
+	}
+	raw_out := generic.RawObject{} 
+	err = s.generic.Delete(ctx, key, &raw_out, filter)
+	if err != nil {
+		return err
+	}
+	err = s.extractObj(raw_out, err, out, false)
+	return err
+}
+
+func(s *genericWrapper) Watch(ctx context.Context, key string, resourceVersion string, filter FilterFunc) (watch.Interface, error) {
+	raw, err := s.generic.Watch(ctx, key, resourceVersion)
+	if err != nil {
+		return nil, err
+	}
+	return newGenericWatcher(raw, s, filter), nil
+}
+
+func(s *genericWrapper) WatchList(ctx context.Context, key string, resourceVersion string, filter FilterFunc) (watch.Interface, error) {
+	raw, err := s.generic.WatchList(ctx, key, resourceVersion)
+	if err != nil {
+		return nil, err
+	}
+	return newGenericWatcher(raw, s, filter), nil
+}
+
+
+func(s *genericWrapper) Get(ctx context.Context, key string, objPtr runtime.Object, ignoreNotFound bool) error {
+	var raw generic.RawObject
+	err := s.generic.Get(ctx, key, &raw, ignoreNotFound)
+	if err != nil {
+		return err
+	}
+	err = s.extractObj(raw, err, objPtr, false)
+	return err
+}
+
+func(s *genericWrapper) GetToList(ctx context.Context, key string, filter FilterFunc, listObj runtime.Object) error {
+	var rawList []generic.RawObject
+	listVersion, err := s.generic.GetToList(ctx, key, &rawList)
+	if err != nil {
+		return err
+	}
+	return s.outputList(key, filter, listObj, listVersion, rawList)
+}
+
+func(s *genericWrapper) List(ctx context.Context, key string, resourceVersion string, filter FilterFunc, listObj runtime.Object) error {
+	var rawList []generic.RawObject
+	listVersion, err := s.generic.List(ctx, key, resourceVersion, &rawList)
+	if err != nil {
+		return err
+	}
+	return s.outputList(key, filter, listObj, listVersion, rawList)
+}
+
+func(s *genericWrapper) outputList(key string, filter FilterFunc, listObj runtime.Object, listVersion uint64, rawList []generic.RawObject) error {
+	listPtr, err := meta.GetItemsPtr(listObj)
+	if err != nil {
+		return err
+	}
+	v, err := conversion.EnforcePtr(listPtr)
+	if err != nil || v.Kind() != reflect.Slice {
+		// This should not happen at runtime.
+		panic("need ptr to slice")
+	}
+	var maxIndex uint64
+	for _, raw := range rawList {
+		obj, _, err := s.codec.Decode(raw.Data, nil, reflect.New(v.Type().Elem()).Interface().(runtime.Object))
+		if err != nil {
+			return err
+		}
+	
+		// being unable to set the version does not prevent the object from being extracted
+		_ = s.versioner.UpdateObject(obj, nil, raw.Version)
+		if filter(obj) {
+			v.Set(reflect.Append(v, reflect.ValueOf(obj).Elem()))
+		}
+		// TODO: contemplate all the possible meanings of the word 'cache'
+		//if kv.ModifyIndex != 0 {
+		//	s.addToCache(kv.ModifyIndex, obj)
+		//}
+		if maxIndex < raw.Version {
+			maxIndex = raw.Version
+		}
+	}
+	if listVersion != 0 {
+		maxIndex = listVersion
+	}
+	if err := s.versioner.UpdateList(listObj, maxIndex); err != nil {
+		return err
+	}
+	return nil
+}
+
+func(s *genericWrapper) GuaranteedUpdate(ctx context.Context, key string, ptrToType runtime.Object, ignoreNotFound bool, preconditions *Preconditions, tryUpdate UpdateFunc) error {
+	if ctx == nil {
+		glog.Errorf("Context is nil")
+	}
+	v, err := conversion.EnforcePtr(ptrToType)
+	if err != nil {
+		// Panic is appropriate, because this is a programming error.
+		panic("need ptr to type")
+	}
+	key = s.prefixKey(key)
+	UpdateFuncWrapper := func(raw *generic.RawObject) error {
+		obj := reflect.New(v.Type()).Interface().(runtime.Object)
+		err := s.extractObj(*raw, nil, obj, ignoreNotFound)
+		if err != nil {
+			return err
+		}
+		if err := checkPreconditions(key, raw.Version, preconditions, obj); err != nil {
+			return err
+		}
+		meta := ResponseMeta{
+			TTL:                raw.TTL,
+			Expiration:         nil, // TODO: translate ttl to expiration
+			ResourceVersion:    raw.Version,
+		}
+		outObj, newTTL, err := tryUpdate(obj, meta)
+		if err != nil {
+			return err
+		}
+		if newTTL != nil {
+			raw.TTL = int64(*newTTL)
+		}
+		if err := s.versioner.UpdateObject(outObj, meta.Expiration, 0); err != nil {
+			return errors.New("resourceVersion cannot be set on objects store in kv")
+		}
+
+		data, err := runtime.Encode(s.codec, outObj)
+		if err != nil {
+			return err
+		}
+		raw.Data = data
+		return nil
+	}
+	return s.generic.GuaranteedUpdate(ctx, key, ignoreNotFound, UpdateFuncWrapper)
+}
+
+func(s *genericWrapper) Codec() runtime.Codec {
+	return s.codec
+}
+
+func (s *genericWrapper) extractObj(raw generic.RawObject, inErr error, objPtr runtime.Object, ignoreNotFound bool) error {
+	if inErr != nil || len(raw.Data) == 0 {
+		if ignoreNotFound {
+			v, err := conversion.EnforcePtr(objPtr)
+			if err != nil {
+				return err
+			}
+			v.Set(reflect.Zero(v.Type()))
+			return nil
+		} else if inErr != nil {
+			return inErr
+		}
+		return fmt.Errorf("unable to locate a value on the RawObject: %#v", raw)
+	}
+	out, gvk, err := s.codec.Decode([]byte(raw.Data), nil, objPtr)
+	if err != nil {
+		return err
+	}
+	if out != objPtr {
+		return fmt.Errorf("unable to decode object %s into %v", gvk.String(), reflect.TypeOf(objPtr))
+	}
+	// being unable to set the version does not prevent the object from being extracted
+	_ = s.versioner.UpdateObject(objPtr, nil, raw.Version)
+	return err
+}
+
+func (s *genericWrapper) prefixKey(key string) string {
+	if strings.HasPrefix(key, s.pathPrefix) {
+		return key
+	}
+	return path.Join(s.pathPrefix, key)
+}
+
+type genericWatcher struct {
+	resultChan  chan watch.Event
+	stopChan    chan bool
+	stopped     uint32
+	raw         generic.InterfaceRawWatch
+	storage     *genericWrapper
+}
+
+func newGenericWatcher(raw generic.InterfaceRawWatch, storage *genericWrapper, filter FilterFunc) *genericWatcher {
+	ret := &genericWatcher{
+		resultChan: make(chan watch.Event),
+		stopChan:   make(chan bool),
+		raw:        raw,
+		storage:    storage,
+	}
+	go ret.run()
+	return ret
+}
+
+func(w *genericWatcher) run() {
+	defer w.cleanup()
+	internalResultChan := w.raw.ResultChan()
+	var evIn generic.RawEvent
+	for {
+		select {
+			case <-w.stopChan:
+				return
+				
+			case evIn = <-internalResultChan:
+				var evOut watch.Event
+				evOut.Type = evIn.Type
+				//TODO: communication of error object types
+				//if evIn.Type == watch.Error {
+				//	decode evIn.Raw.Data without storage.codec
+				//} else
+				if len(evIn.Raw.Data) > 0 {
+					obj, err := runtime.Decode(w.storage.codec, evIn.Raw.Data)
+					if err != nil {
+						//TODO: glog
+						continue
+					}
+					evOut.Object = obj
+				}
+				select {
+					case <-w.stopChan:
+						return
+						
+					case w.resultChan<-evOut:
+				}
+		}
+	}
+}
+
+func(w *genericWatcher) cleanup() {
+	close(w.resultChan)
+	close(w.stopChan)
+}
+
+func(w *genericWatcher) Stop() {
+	if atomic.SwapUint32( &w.stopped, 1 ) == 0 {
+		w.raw.Stop()
+		w.stopChan <- true
+	}
+}
+
+func(w *genericWatcher) ResultChan() <-chan watch.Event {
+	return w.resultChan
+}
+
+type APIObjectVersioner struct{}
+
+// UpdateObject implements Versioner
+func (a APIObjectVersioner) UpdateObject(obj runtime.Object, expiration *time.Time, resourceVersion uint64) error {
+	accessor, err := meta.Accessor(obj)
+	if err != nil {
+		return err
+	}
+	if expiration != nil {
+		accessor.SetDeletionTimestamp(&unversioned.Time{Time: *expiration})
+	}
+	versionString := ""
+	if resourceVersion != 0 {
+		versionString = strconv.FormatUint(resourceVersion, 10)
+	}
+	accessor.SetResourceVersion(versionString)
+	return nil
+}
+
+// UpdateList implements Versioner
+func (a APIObjectVersioner) UpdateList(obj runtime.Object, resourceVersion uint64) error {
+	listMeta, err := api.ListMetaFor(obj)
+	if err != nil || listMeta == nil {
+		return err
+	}
+	versionString := ""
+	if resourceVersion != 0 {
+		versionString = strconv.FormatUint(resourceVersion, 10)
+	}
+	listMeta.ResourceVersion = versionString
+	return nil
+}
+
+// ObjectResourceVersion implements Versioner
+func (a APIObjectVersioner) ObjectResourceVersion(obj runtime.Object) (uint64, error) {
+	accessor, err := meta.Accessor(obj)
+	if err != nil {
+		return 0, err
+	}
+	version := accessor.GetResourceVersion()
+	if len(version) == 0 {
+		return 0, nil
+	}
+	return strconv.ParseUint(version, 10, 64)
+}
+
+func getTypeName(obj interface{}) string {
+	return reflect.TypeOf(obj).String()
+}
+
+func checkPreconditions(key string, rv uint64, preconditions *Preconditions, out runtime.Object) error {
+	if preconditions == nil {
+		return nil
+	}
+	objMeta, err := api.ObjectMetaFor(out)
+	if err != nil {
+		return NewInternalErrorf("can't enforce preconditions %v on un-introspectable object %v, got error: %v", *preconditions, out, err)
+	}
+	if preconditions.UID != nil && *preconditions.UID != objMeta.UID {
+		return NewResourceVersionConflictsError(key, int64(rv)) 
+	}
+	return nil
+}
+
+// APIObjectVersioner implements Versioner
+var _ Versioner = APIObjectVersioner{}
+var _ Interface = &genericWrapper{}
+var _ watch.Interface = &genericWatcher{}
