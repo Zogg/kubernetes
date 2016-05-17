00e2bd93589b15d635b54d91bccb134a518fbfee
Gonzalo Maldonado <elg0nz@gmail.com>
2016-05-17 16:59:48 -0700

    Add first real test to kube2consul

--- cluster/addons/dns/kube2consul/kube2consul_test.go
+++ cluster/addons/dns/kube2consul/kube2consul_test.go
@@ -17,67 +17,13 @@ limitations under the License.
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
-	"path"
-	"strings"
-	"testing"
-	"time"
-
-	"github.com/coreos/go-etcd/etcd"
 	consulApi "github.com/hashicorp/consul/api"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	kapi "k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/client/cache"
+	"testing"
 )
-
-type fakeEtcdClient struct {
-	// TODO: Convert this to real fs to better simulate etcd behavior.
-	writes map[string]string
-}
-
-func (ec *fakeEtcdClient) Set(key, value string, ttl uint64) (*etcd.Response, error) {
-	ec.writes[key] = value
-	return nil, nil
-}
-
-func (ec *fakeEtcdClient) Delete(key string, recursive bool) (*etcd.Response, error) {
-	for p := range ec.writes {
-		if (recursive && strings.HasPrefix(p, key)) || (!recursive && p == key) {
-			delete(ec.writes, p)
-		}
-	}
-	return nil, nil
-}
-
-func (ec *fakeEtcdClient) RawGet(key string, sort, recursive bool) (*etcd.RawResponse, error) {
-	values := ec.Get(key)
-	if len(values) == 0 {
-		return &etcd.RawResponse{StatusCode: http.StatusNotFound}, nil
-	}
-	return &etcd.RawResponse{StatusCode: http.StatusOK}, nil
-}
-
-func (ec *fakeEtcdClient) Get(key string) []string {
-	values := make([]string, 0, 10)
-	minSeparatorCount := 0
-	key = strings.ToLower(key)
-	for path := range ec.writes {
-		if strings.HasPrefix(path, key) {
-			separatorCount := strings.Count(path, "/")
-			if minSeparatorCount == 0 || separatorCount < minSeparatorCount {
-				minSeparatorCount = separatorCount
-				values = values[:0]
-				values = append(values, ec.writes[path])
-			} else if separatorCount == minSeparatorCount {
-				values = append(values, ec.writes[path])
-			}
-		}
-	}
-	return values
-}
 
 type fakeConsulAgent struct {
 	// TODO: Convert this to real fs to better simulate consul behavior.
@@ -91,32 +37,12 @@ func (fa *fakeConsulAgent) ServiceRegister(service *consulApi.AgentServiceRegist
 	return nil
 }
 
-const (
-	testDomain       = "cluster.local."
-	basePath         = "/skydns/local/cluster"
-	serviceSubDomain = "svc"
-	podSubDomain     = "pod"
-)
-
-func newKube2Consul(ec etcdClient, ca consulAgent) *kube2consul {
-	return &kube2consul{
-		// TODO: Abstract this so it allows consul or etcd clients.
-		etcdClient:          ec,
-		consulAgent:         ca,
-		domain:              testDomain,
-		etcdMutationTimeout: time.Second,
-		endpointsStore:      cache.NewStore(cache.MetaNamespaceKeyFunc),
-		servicesStore:       cache.NewStore(cache.MetaNamespaceKeyFunc),
-	}
+func (fa *fakeConsulAgent) ServiceDeregister(serviceID string) error {
+	delete(fa.writes, serviceID)
+	return nil
 }
 
-func getEtcdPathForA(name, namespace, subDomain string) string {
-	return path.Join(basePath, subDomain, namespace, name)
-}
-
-func getEtcdPathForSRV(portName, protocol, name, namespace string) string {
-	return path.Join(basePath, serviceSubDomain, namespace, name, fmt.Sprintf("_%s", strings.ToLower(protocol)), fmt.Sprintf("_%s", strings.ToLower(portName)))
-}
+// FIXME: instead of duplicating, extract these methods from kube2sky tests
 
 type hostPort struct {
 	Host string `json:"host"`
@@ -126,65 +52,9 @@ type hostPort struct {
 func getHostPort(service *kapi.Service) *hostPort {
 	return &hostPort{
 		Host: service.Spec.ClusterIP,
-		Port: service.Spec.Ports[0].Port,
+		Port: int(service.Spec.Ports[0].Port),
 	}
 }
-
-func getHostPortFromString(data string) (*hostPort, error) {
-	var res hostPort
-	err := json.Unmarshal([]byte(data), &res)
-	return &res, err
-}
-
-func assertDnsServiceEntryInEtcd(t *testing.T, fc *fakeEtcdClient, serviceName, namespace string, expectedHostPort *hostPort) {
-	key := getEtcdPathForA(serviceName, namespace, serviceSubDomain)
-	values := fc.Get(key)
-	//require.True(t, exists)
-	require.True(t, len(values) > 0, "entry not found.")
-	actualHostPort, err := getHostPortFromString(values[0])
-	require.NoError(t, err)
-	assert.Equal(t, expectedHostPort.Host, actualHostPort.Host)
-}
-
-func assertDnsPodEntryInEtcd(t *testing.T, fc *fakeEtcdClient, podIP, namespace string) {
-	key := getEtcdPathForA(podIP, namespace, podSubDomain)
-	values := fc.Get(key)
-	require.True(t, len(values) > 0, "entry not found.")
-}
-
-func assertDnsPodEntryNotInEtcd(t *testing.T, fc *fakeEtcdClient, podIP, namespace string) {
-	key := getEtcdPathForA(podIP, namespace, podSubDomain)
-	values := fc.Get(key)
-	require.True(t, len(values) == 0, "entry found.")
-}
-
-func assertSRVEntryInEtcd(t *testing.T, fc *fakeEtcdClient, portName, protocol, serviceName, namespace string, expectedPortNumber, expectedEntriesCount int) {
-	srvKey := getEtcdPathForSRV(portName, protocol, serviceName, namespace)
-	values := fc.Get(srvKey)
-	assert.Equal(t, expectedEntriesCount, len(values))
-	for i := range values {
-		actualHostPort, err := getHostPortFromString(values[i])
-		require.NoError(t, err)
-		assert.Equal(t, expectedPortNumber, actualHostPort.Port)
-	}
-}
-
-func newHeadlessService(namespace, serviceName string) kapi.Service {
-	service := kapi.Service{
-		ObjectMeta: kapi.ObjectMeta{
-			Name:      serviceName,
-			Namespace: namespace,
-		},
-		Spec: kapi.ServiceSpec{
-			ClusterIP: "None",
-			Ports: []kapi.ServicePort{
-				{Port: 0},
-			},
-		},
-	}
-	return service
-}
-
 func newService(namespace, serviceName, clusterIP, portName string, portNumber int) kapi.Service {
 	service := kapi.Service{
 		ObjectMeta: kapi.ObjectMeta{
@@ -194,279 +64,61 @@ func newService(namespace, serviceName, clusterIP, portName string, portNumber i
 		Spec: kapi.ServiceSpec{
 			ClusterIP: clusterIP,
 			Ports: []kapi.ServicePort{
-				{Port: portNumber, Name: portName, Protocol: "TCP"},
+				{Port: int32(portNumber), Name: portName, Protocol: "TCP"},
 			},
 		},
 	}
 	return service
 }
 
-func newPod(namespace, podName, podIP string) kapi.Pod {
-	pod := kapi.Pod{
-		ObjectMeta: kapi.ObjectMeta{
-			Name:      podName,
-			Namespace: namespace,
-		},
-		Status: kapi.PodStatus{
-			PodIP: podIP,
-		},
+const (
+	testDomain       = "cluster.local."
+	basePath         = "/skydns/local/cluster"
+	serviceSubDomain = "svc"
+	podSubDomain     = "pod"
+)
+
+type fakeConsulKV struct {
+	writes map[string]*consulApi.KVPair
+}
+
+func (fk *fakeConsulKV) Get(key string, q *consulApi.QueryOptions) (*consulApi.KVPair, *consulApi.QueryMeta, error) {
+	pair := fk.writes[key]
+	return pair, nil, nil
+}
+
+func (fk *fakeConsulKV) Put(p *consulApi.KVPair, q *consulApi.WriteOptions) (*consulApi.WriteMeta, error) {
+	key := p.Key
+	fk.writes[key] = p
+	return nil, nil
+}
+
+func (fk *fakeConsulKV) Delete(key string, w *consulApi.WriteOptions) (*consulApi.WriteMeta, error) {
+	delete(fk.writes, key)
+	return nil, nil
+}
+
+func newKube2Consul(ca *fakeConsulAgent, ck *fakeConsulKV) *kube2consul {
+	return &kube2consul{
+		consulAgent:    ca,
+		consulKV:       ck,
+		domain:         testDomain,
+		endpointsStore: cache.NewStore(cache.MetaNamespaceKeyFunc),
+		servicesStore:  cache.NewStore(cache.MetaNamespaceKeyFunc),
 	}
-
-	return pod
 }
-
-func newSubset() kapi.EndpointSubset {
-	subset := kapi.EndpointSubset{
-		Addresses: []kapi.EndpointAddress{},
-		Ports:     []kapi.EndpointPort{},
-	}
-	return subset
-}
-
-func newSubsetWithOnePort(portName string, port int, ips ...string) kapi.EndpointSubset {
-	subset := newSubset()
-	subset.Ports = append(subset.Ports, kapi.EndpointPort{Port: port, Name: portName, Protocol: "TCP"})
-	for _, ip := range ips {
-		subset.Addresses = append(subset.Addresses, kapi.EndpointAddress{IP: ip})
-	}
-	return subset
-}
-
-func newSubsetWithTwoPorts(portName1 string, portNumber1 int, portName2 string, portNumber2 int, ips ...string) kapi.EndpointSubset {
-	subset := newSubsetWithOnePort(portName1, portNumber1, ips...)
-	subset.Ports = append(subset.Ports, kapi.EndpointPort{Port: portNumber2, Name: portName2, Protocol: "TCP"})
-	return subset
-}
-
-func newEndpoints(service kapi.Service, subsets ...kapi.EndpointSubset) kapi.Endpoints {
-	endpoints := kapi.Endpoints{
-		ObjectMeta: service.ObjectMeta,
-		Subsets:    []kapi.EndpointSubset{},
-	}
-
-	for _, subset := range subsets {
-		endpoints.Subsets = append(endpoints.Subsets, subset)
-	}
-	return endpoints
-}
-
-/*
-func TestHeadlessService(t *testing.T) {
-	const (
-		testService   = "testservice"
-		testNamespace = "default"
-	)
-	fa := &fakeConsulAgent{make(map[string]string)}
-	fe := &fakeEtcdClient{make(map[string]string)}
-	k2c := newKube2Consul(fe, fa)
-	service := newHeadlessService(testNamespace, testService)
-	assert.NoError(t, k2c.servicesStore.Add(&service))
-	endpoints := newEndpoints(service, newSubsetWithOnePort("", 80, "10.0.0.1", "10.0.0.2"), newSubsetWithOnePort("", 8080, "10.0.0.3", "10.0.0.4"))
-
-	// We expect 4 records.
-	expectedDNSRecords := 4
-	assert.NoError(t, k2c.endpointsStore.Add(&endpoints))
-	k2c.newService(&service)
-	assert.Equal(t, expectedDNSRecords, len(fe.writes))
-	assert.Equal(t, expectedDNSRecords, len(fa.writes))
-	k2c.removeService(&service)
-	assert.Empty(t, fe.writes)
-}
-
-func TestHeadlessServiceWithNamedPorts(t *testing.T) {
-	const (
-		testService   = "testservice"
-		testNamespace = "default"
-	)
-	ec := &fakeEtcdClient{make(map[string]string)}
-	k2c := newKube2Consul(ec)
-	service := newHeadlessService(testNamespace, testService)
-	assert.NoError(t, k2c.servicesStore.Add(&service))
-	endpoints := newEndpoints(service, newSubsetWithTwoPorts("http1", 80, "http2", 81, "10.0.0.1", "10.0.0.2"), newSubsetWithOnePort("https", 443, "10.0.0.3", "10.0.0.4"))
-
-	// We expect 10 records. 6 SRV records. 4 POD records.
-	expectedDNSRecords := 10
-	assert.NoError(t, k2c.endpointsStore.Add(&endpoints))
-	k2c.newService(&service)
-	assert.Equal(t, expectedDNSRecords, len(ec.writes))
-	assertSRVEntryInEtcd(t, ec, "http1", "tcp", testService, testNamespace, 80, 2)
-	assertSRVEntryInEtcd(t, ec, "http2", "tcp", testService, testNamespace, 81, 2)
-	assertSRVEntryInEtcd(t, ec, "https", "tcp", testService, testNamespace, 443, 2)
-
-	endpoints.Subsets = endpoints.Subsets[:1]
-	k2c.handleEndpointAdd(&endpoints)
-	// We expect 6 records. 4 SRV records. 2 POD records.
-	expectedDNSRecords = 6
-	assert.Equal(t, expectedDNSRecords, len(ec.writes))
-	assertSRVEntryInEtcd(t, ec, "http1", "tcp", testService, testNamespace, 80, 2)
-	assertSRVEntryInEtcd(t, ec, "http2", "tcp", testService, testNamespace, 81, 2)
-
-	k2c.removeService(&service)
-	assert.Empty(t, ec.writes)
-}
-
-func TestHeadlessServiceEndpointsUpdate(t *testing.T) {
-	const (
-		testService   = "testservice"
-		testNamespace = "default"
-	)
-	ec := &fakeEtcdClient{make(map[string]string)}
-	k2c := newKube2Consul(ec)
-	service := newHeadlessService(testNamespace, testService)
-	assert.NoError(t, k2c.servicesStore.Add(&service))
-	endpoints := newEndpoints(service, newSubsetWithOnePort("", 80, "10.0.0.1", "10.0.0.2"))
-
-	expectedDNSRecords := 2
-	assert.NoError(t, k2c.endpointsStore.Add(&endpoints))
-	k2c.newService(&service)
-	assert.Equal(t, expectedDNSRecords, len(ec.writes))
-	endpoints.Subsets = append(endpoints.Subsets,
-		newSubsetWithOnePort("", 8080, "10.0.0.3", "10.0.0.4"),
-	)
-	expectedDNSRecords = 4
-	k2c.handleEndpointAdd(&endpoints)
-
-	assert.Equal(t, expectedDNSRecords, len(ec.writes))
-	k2c.removeService(&service)
-	assert.Empty(t, ec.writes)
-}
-
-func TestHeadlessServiceWithDelayedEndpointsAddition(t *testing.T) {
-	const (
-		testService   = "testservice"
-		testNamespace = "default"
-	)
-	ec := &fakeEtcdClient{make(map[string]string)}
-	k2c := newKube2Consul(ec)
-	service := newHeadlessService(testNamespace, testService)
-	assert.NoError(t, k2c.servicesStore.Add(&service))
-	// Headless service DNS records should not be created since
-	// corresponding endpoints object doesn't exist.
-	k2c.newService(&service)
-	assert.Empty(t, ec.writes)
-
-	// Add an endpoints object for the service.
-	endpoints := newEndpoints(service, newSubsetWithOnePort("", 80, "10.0.0.1", "10.0.0.2"), newSubsetWithOnePort("", 8080, "10.0.0.3", "10.0.0.4"))
-	// We expect 4 records.
-	expectedDNSRecords := 4
-	k2c.handleEndpointAdd(&endpoints)
-	assert.Equal(t, expectedDNSRecords, len(ec.writes))
-}
-
-// TODO: Test service updates for headless services.
-// TODO: Test headless service addition with delayed endpoints addition
 
 func TestAddSinglePortService(t *testing.T) {
 	const (
 		testService   = "testservice"
 		testNamespace = "default"
 	)
-	ec := &fakeEtcdClient{make(map[string]string)}
-	k2c := newKube2Consul(ec)
+	fck := &fakeConsulKV{make(map[string]*consulApi.KVPair)}
+	fca := &fakeConsulAgent{make(map[string]string)}
+	k2c := newKube2Consul(fca, fck)
 	service := newService(testNamespace, testService, "1.2.3.4", "", 0)
 	k2c.newService(&service)
-	expectedValue := getHostPort(&service)
-	assertDnsServiceEntryInEtcd(t, ec, testService, testNamespace, expectedValue)
+	// expectedValue := getHostPort(&service)
+
+	assert.Equal(t, 1, len(fck.writes))
 }
-
-func TestUpdateSinglePortService(t *testing.T) {
-	const (
-		testService   = "testservice"
-		testNamespace = "default"
-	)
-	ec := &fakeEtcdClient{make(map[string]string)}
-	k2c := newKube2Consul(ec)
-	service := newService(testNamespace, testService, "1.2.3.4", "", 0)
-	k2c.newService(&service)
-	assert.Len(t, ec.writes, 1)
-	newService := service
-	newService.Spec.ClusterIP = "0.0.0.0"
-	k2c.updateService(&service, &newService)
-	expectedValue := getHostPort(&newService)
-	assertDnsServiceEntryInEtcd(t, ec, testService, testNamespace, expectedValue)
-}
-
-func TestDeleteSinglePortService(t *testing.T) {
-	const (
-		testService   = "testservice"
-		testNamespace = "default"
-	)
-	ec := &fakeEtcdClient{make(map[string]string)}
-	k2c := newKube2Consul(ec)
-	service := newService(testNamespace, testService, "1.2.3.4", "", 80)
-	// Add the service
-	k2c.newService(&service)
-	assert.Len(t, ec.writes, 1)
-	// Delete the service
-	k2c.removeService(&service)
-	assert.Empty(t, ec.writes)
-}
-
-func TestServiceWithNamePort(t *testing.T) {
-	const (
-		testService   = "testservice"
-		testNamespace = "default"
-	)
-	ec := &fakeEtcdClient{make(map[string]string)}
-	k2c := newKube2Consul(ec)
-
-	// create service
-	service := newService(testNamespace, testService, "1.2.3.4", "http1", 80)
-	k2c.newService(&service)
-	expectedValue := getHostPort(&service)
-	assertDnsServiceEntryInEtcd(t, ec, testService, testNamespace, expectedValue)
-	assertSRVEntryInEtcd(t, ec, "http1", "tcp", testService, testNamespace, 80, 1)
-	assert.Len(t, ec.writes, 2)
-
-	// update service
-	newService := service
-	newService.Spec.Ports[0].Name = "http2"
-	k2c.updateService(&service, &newService)
-	expectedValue = getHostPort(&newService)
-	assertDnsServiceEntryInEtcd(t, ec, testService, testNamespace, expectedValue)
-	assertSRVEntryInEtcd(t, ec, "http2", "tcp", testService, testNamespace, 80, 1)
-	assert.Len(t, ec.writes, 2)
-
-	// Delete the service
-	k2c.removeService(&service)
-	assert.Empty(t, ec.writes)
-}
-
-func TestPodDns(t *testing.T) {
-	const (
-		testPodIP      = "1.2.3.4"
-		sanitizedPodIP = "1-2-3-4"
-		testNamespace  = "default"
-		testPodName    = "testPod"
-	)
-	ec := &fakeEtcdClient{make(map[string]string)}
-	k2c := newKube2Consul(ec)
-
-	// create pod without ip address yet
-	pod := newPod(testNamespace, testPodName, "")
-	k2c.handlePodCreate(&pod)
-	assert.Empty(t, ec.writes)
-
-	// create pod
-	pod = newPod(testNamespace, testPodName, testPodIP)
-	k2c.handlePodCreate(&pod)
-	assertDnsPodEntryInEtcd(t, ec, sanitizedPodIP, testNamespace)
-
-	// update pod with same ip
-	newPod := pod
-	newPod.Status.PodIP = testPodIP
-	k2c.handlePodUpdate(&pod, &newPod)
-	assertDnsPodEntryInEtcd(t, ec, sanitizedPodIP, testNamespace)
-
-	// update pod with different ip's
-	newPod = pod
-	newPod.Status.PodIP = "4.3.2.1"
-	k2c.handlePodUpdate(&pod, &newPod)
-	assertDnsPodEntryInEtcd(t, ec, "4-3-2-1", testNamespace)
-	assertDnsPodEntryNotInEtcd(t, ec, "1-2-3-4", testNamespace)
-
-	// Delete the pod
-	k2c.handlePodDelete(&newPod)
-	assert.Empty(t, ec.writes)
-}
-*/
